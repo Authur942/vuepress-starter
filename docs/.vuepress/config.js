351544ce8435f843b73400a82d7475839eb79842
zhanggangbin <1738238581@qq.com>
2022-03-06 02:12:52 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports ={
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/Authur942'
+      },
+      {
+        text: 'Blog',
+        link: 'https://www.cnblogs.com/KleinBlue/'
+      }
+    ]
+  }
+}
